252e7c2f0156ddce1bb2df7ad7d908a3dcf5cb06
Peter <peterebutler@gmail.com>
2024-02-01 16:59:16 -0700

    Remove some boilerplate methods not currently needed.

--- tap_woo/client.py
+++ tap_woo/client.py
@@ -32,9 +32,6 @@ class wooStream(RESTStream):
 
     records_jsonpath = "$[*]"  # Or override `parse_response`.
 
-    # # Set this value or override `get_new_paginator`.
-    # next_page_token_jsonpath = "$.page"  # noqa: S105
-
     @property
     def authenticator(self) -> BasicAuthenticator:
         """Return a new authenticator object.
@@ -77,25 +74,6 @@ class wooStream(RESTStream):
         """
         return super().get_new_paginator()
 
-    def prepare_request_payload(
-        self,
-        context: dict | None,  # noqa: ARG002
-        next_page_token: Any | None,  # noqa: ARG002, ANN401
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
-
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result records.
 
@@ -107,23 +85,6 @@ class wooStream(RESTStream):
         """
         # TODO: Parse response body and return a set of records.
         yield from extract_jsonpath(self.records_jsonpath, input=response.json())
-
-    def post_process(
-        self,
-        row: dict,
-        context: dict | None = None,  # noqa: ARG002
-    ) -> dict | None:
-        """As needed, append or transform raw data to match expected structure.
-
-        Args:
-            row: An individual record from the stream.
-            context: The stream context.
-
-        Returns:
-            The updated record dictionary, or ``None`` to skip the record.
-        """
-        # TODO: Delete this method if not needed.
-        return row
     
     def get_url_params(self, context, next_page_token):
         self.logger.info(f"Next page token: {next_page_token}")
